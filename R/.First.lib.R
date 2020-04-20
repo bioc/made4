b80db8f4aea1a0fcf6f1dbbc079c2aa211826387
aedin <aedin@jimmy.harvard.edu>
2020-04-20 17:25:32 -0400

    NAMESPACE update

--- R/.First.lib.R
+++
@@ -1,9 +0,0 @@
-.First.lib <- function(libname, pkgname) {
-
-   # Only works for windows now
-    if(interactive() && .Platform$OS.type == "windows" &&
-             .Platform.GUI == "Rgui"){
-        addVigs2WinMenu("ade4")
-    }
-}
-
